59dcbb7554db277b8cae0f279e8f2a05f3cef86b
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2019-05-30 16:03:32 -0700

    added the ability to specify if a pointer is on by default (#41)

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/BaseControllerPointer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/BaseControllerPointer.cs
@@ -54,6 +54,10 @@ namespace XRTK.SDK.UX.Pointers
         [Tooltip("Does the interaction require hold?")]
         private bool requiresHoldAction = false;
 
+        [SerializeField]
+        [Tooltip("Enables the pointer ray when the pointer is started.")]
+        private bool enablePointerOnStart = false;
+
         /// <summary>
         /// True if select is pressed right now
         /// </summary>
@@ -111,11 +115,7 @@ namespace XRTK.SDK.UX.Pointers
                     BaseCursor.DefaultCursorDistance = PointerExtent;
                     BaseCursor.Pointer = this;
                     BaseCursor.SetVisibilityOnSourceDetected = setCursorVisibilityOnSourceDetected;
-
-                    if (disableCursorOnStart)
-                    {
-                        BaseCursor.SetVisibility(false);
-                    }
+                    BaseCursor.SetVisibility(!disableCursorOnStart);
                 }
                 else
                 {
@@ -249,7 +249,7 @@ namespace XRTK.SDK.UX.Pointers
                     return true;
                 }
 
-                return HasSelectPressedOnce;
+                return HasSelectPressedOnce || enablePointerOnStart;
             }
         }
 
