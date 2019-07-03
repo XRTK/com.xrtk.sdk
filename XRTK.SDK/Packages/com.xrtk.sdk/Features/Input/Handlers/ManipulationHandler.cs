cfefe97256a2a6a7d8c168d0f71de623188af4a0
Stephen Hodgson <hodgson.designs@gmail.com>
2019-07-03 07:12:07 -0400

    removed a bunch of stuff to get a better understanding of what was needed

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/Input/Handlers/ManipulationHandler.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/Input/Handlers/ManipulationHandler.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Ara3D.Experimental;
 using UnityEngine;
 using XRTK.Definitions.InputSystem;
 using XRTK.EventDatum.Input;
 using XRTK.Interfaces.InputSystem;
 using XRTK.Interfaces.InputSystem.Handlers;
-using XRTK.Services;
 
 namespace XRTK.SDK.Input.Handlers
 {
@@ -16,7 +16,6 @@ namespace XRTK.SDK.Input.Handlers
     /// nudge, rotate, and scale the object.
     /// </summary>
     public class ManipulationHandler : BaseInputHandler,
-        IMixedRealityPointerHandler,
         IMixedRealityInputHandler,
         IMixedRealityInputHandler<Vector2>
     {
@@ -100,16 +99,6 @@ namespace XRTK.SDK.Input.Handlers
             set => scaleAmount = value;
         }
 
-        //[PhysicsLayer]
-        //[SerializeField]
-        //[Tooltip("The physics layer to place the object in while doing manipulations")]
-        //private int manipulationLayer = 0;
-
-        ///// <summary>
-        ///// The default physics layer this <see cref="GameObject"/> is usually on when not in the manipulation phase.
-        ///// </summary>
-        //private int defaultLayer = 0;
-
         #endregion Manipulation Options
 
         /// <summary>
@@ -125,29 +114,6 @@ namespace XRTK.SDK.Input.Handlers
         /// </summary>
         private IMixedRealityInputSource primaryInputSource = null;
 
-        /// <summary>
-        /// The first pointer to start the manipulation phase of this object.
-        /// </summary>
-        private IMixedRealityPointer primaryPointer = null;
-
-        ///// <summary>
-        ///// The second input source detected during the manipulation phase of this object.
-        ///// </summary>
-        ///// <remarks>
-        ///// Used for two handed manipulations
-        ///// </remarks>
-        //private IMixedRealityInputSource secondaryInputSource = null;
-
-        ///// <summary>
-        ///// The second pointer detected during the manipulation phase of this object.
-        ///// </summary>
-        //private IMixedRealityPointer secondaryPointer = null;
-
-        ///// <summary>
-        ///// The collider used when doing manipulations.
-        ///// </summary>
-        //private Collider manipulationCollider = null;
-
         #region Monobehaviour Implementation
 
         private void Awake()
@@ -158,78 +124,7 @@ namespace XRTK.SDK.Input.Handlers
             }
         }
 
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-        }
-
-        private void Update()
-        {
-            if (isBeingHeld)
-            {
-                transform.position = primaryPointer != null
-                    ? primaryPointer.Result.Details.Point
-                    : MixedRealityToolkit.InputSystem.GazeProvider.GazePointer.Result.Details.Point;
-            }
-        }
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-        }
-
         #endregion Monobehaviour Implementation
-
-        #region IMixedRealityPointerHandler Implementation
-
-        /// <inheritdoc />
-        public virtual void OnPointerDown(MixedRealityPointerEventData eventData)
-        {
-            if (eventData.MixedRealityInputAction == selectAction)
-            {
-                if (primaryInputSource == null)
-                {
-                    primaryPointer = eventData.Pointer;
-                    primaryInputSource = eventData.InputSource;
-                }
-                //else if (secondaryInputSource == null)
-                //{
-                //    secondaryPointer = eventData.Pointer;
-                //    secondaryInputSource = eventData.InputSource;
-                //}
-            }
-        }
-
-        /// <inheritdoc />
-        public virtual void OnPointerUp(MixedRealityPointerEventData eventData)
-        {
-            if (eventData.MixedRealityInputAction == selectAction)
-            {
-                // var resetSecondary = false;
-
-                if (primaryInputSource != null &&
-                    primaryInputSource.SourceId == eventData.InputSource.SourceId)
-                {
-                    // Clear both primary and secondary input sources.
-                    primaryPointer = null;
-                    primaryInputSource = null;
-                    // resetSecondary = true;
-                }
-
-                //if (secondaryInputSource != null &&
-                //    (resetSecondary ||
-                //     eventData.InputSource.SourceId == secondaryInputSource.SourceId))
-                //{
-                //    secondaryPointer = null;
-                //    secondaryInputSource = null;
-                //}
-            }
-        }
-
-        /// <inheritdoc />
-        public virtual void OnPointerClicked(MixedRealityPointerEventData eventData) { }
-
-        #endregion IMixedRealityPointerHandler Implementation
 
         #region IMixedRealityInputHandler Implementation
 
@@ -238,14 +133,10 @@ namespace XRTK.SDK.Input.Handlers
         {
             if (eventData.MixedRealityInputAction == selectAction)
             {
-                if (primaryInputSource == null)
+                if (!useHold)
                 {
-                    primaryInputSource = eventData.InputSource;
+                    BeginHold(eventData);
                 }
-                //else if (secondaryInputSource == null)
-                //{
-                //    secondaryInputSource = eventData.InputSource;
-                //}
             }
         }
 
@@ -254,24 +145,15 @@ namespace XRTK.SDK.Input.Handlers
         {
             if (eventData.MixedRealityInputAction == selectAction)
             {
-                // var resetSecondary = false;
-
-                if (primaryInputSource != null &&
-                    primaryInputSource.SourceId == eventData.InputSource.SourceId)
+                if (useHold && !isBeingHeld)
                 {
-                    // Clear both primary and secondary input sources.
-                    primaryPointer = null;
-                    primaryInputSource = null;
-                    // resetSecondary = true;
+                    BeginHold(eventData);
                 }
 
-                //if (secondaryInputSource != null &&
-                //    (resetSecondary ||
-                //     eventData.InputSource.SourceId == secondaryInputSource.SourceId))
-                //{
-                //    secondaryPointer = null;
-                //    secondaryInputSource = null;
-                //}
+                if (!useHold && isBeingHeld)
+                {
+                    EndHold(eventData);
+                }
             }
         }
 
@@ -279,14 +161,6 @@ namespace XRTK.SDK.Input.Handlers
         public virtual void OnInputChanged(InputEventData<Vector2> eventData)
         {
             if (!isBeingHeld) { return; }
-
-            if (primaryInputSource != null &&
-                eventData.MixedRealityInputAction == rotateAction)
-            {
-                if (eventData.InputSource.SourceId == primaryInputSource.SourceId)
-                {
-                }
-            }
 
             if (primaryInputSource != null &&
                 eventData.MixedRealityInputAction == scaleAction)
@@ -302,5 +176,26 @@ namespace XRTK.SDK.Input.Handlers
         }
 
         #endregion IMixedRealityInputHandler Implementation
+
+        private void BeginHold(InputEventData eventData)
+        {
+            isBeingHeld = true;
+
+            if (primaryInputSource == null)
+            {
+                primaryInputSource = eventData.InputSource;
+            }
+        }
+
+        private void EndHold(InputEventData eventData)
+        {
+            if (primaryInputSource != null &&
+                primaryInputSource.SourceId == eventData.InputSource.SourceId)
+            {
+                primaryInputSource = null;
+            }
+
+            isBeingHeld = false;
+        }
     }
 }
