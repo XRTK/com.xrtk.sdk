ad50851160eeaea78b6d2240a9f18c03c751ffd2
Stephen Hodgson <hodgson.designs@gmail.com>
2019-10-21 16:26:41 -0400

    Associated changes for the Camera System update

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/TeleportPointer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/TeleportPointer.cs
@@ -293,7 +293,7 @@ namespace XRTK.SDK.UX.Pointers
                             {
                                 canMove = false;
                                 // Rotate the camera by the rotation amount.  If our angle is positive then rotate in the positive direction, otherwise in the opposite direction.
-                                MixedRealityToolkit.Instance.MixedRealityPlayspace.RotateAround(CameraCache.Main.transform.position, Vector3.up, angle >= 0.0f ? rotationAmount : -rotationAmount);
+                                MixedRealityToolkit.CameraSystem.CameraRig.PlayspaceTransform.RotateAround(CameraCache.Main.transform.position, Vector3.up, angle >= 0.0f ? rotationAmount : -rotationAmount);
                             }
                             else // We may be trying to strafe backwards.
                             {
@@ -307,11 +307,11 @@ namespace XRTK.SDK.UX.Pointers
                                 if (offsetStrafeAngle > 0 && offsetStrafeAngle < backStrafeActivationAngle)
                                 {
                                     canMove = false;
-                                    var playspacePosition = MixedRealityToolkit.Instance.MixedRealityPlayspace.position;
+                                    var playspacePosition = MixedRealityToolkit.CameraSystem.CameraRig.PlayspaceTransform.position;
                                     var height = playspacePosition.y;
                                     var newPosition = -CameraCache.Main.transform.forward * strafeAmount + playspacePosition;
                                     newPosition.y = height;
-                                    MixedRealityToolkit.Instance.MixedRealityPlayspace.position = newPosition;
+                                    MixedRealityToolkit.CameraSystem.CameraRig.PlayspaceTransform.position = newPosition;
                                 }
                             }
                         }
