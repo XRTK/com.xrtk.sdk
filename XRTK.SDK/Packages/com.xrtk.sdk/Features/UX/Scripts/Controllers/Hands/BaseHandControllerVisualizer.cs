9bf86d0d573580b78749e2b368d184277eee85ec
Dino Fejzagic <dino.f@live.de>
2019-12-04 18:08:26 +0100

    Implement DefaultHandControllerVisualizer

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Controllers/Hands/BaseHandControllerVisualizer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Controllers/Hands/BaseHandControllerVisualizer.cs
@@ -2,10 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 using XRTK.Definitions.Controllers;
-using XRTK.Definitions.Utilities;
 using XRTK.EventDatum.Input;
 using XRTK.Interfaces.InputSystem.Handlers;
 using XRTK.Interfaces.Providers.Controllers;
@@ -18,7 +16,6 @@ namespace XRTK.SDK.UX.Controllers.Hands
     public abstract class BaseHandControllerVisualizer : ControllerPoseSynchronizer, IMixedRealityControllerVisualizer, IMixedRealityHandDataHandler
     {
         private IMixedRealityHandControllerDataProvider dataProvider;
-        private Dictionary<TrackedHandJoint, Transform> jointTransforms = new Dictionary<TrackedHandJoint, Transform>();
 
         [SerializeField]
         [Tooltip("Should a gizmo be drawn to represent the hand bounds.")]
@@ -39,16 +36,6 @@ namespace XRTK.SDK.UX.Controllers.Hands
                 }
             }
         }
-
-        /// <summary>
-        /// The mesh filter used for visualization.
-        /// </summary>
-        protected MeshFilter MeshFilter { get; private set; }
-
-        /// <summary>
-        /// Provides read-only access to the joint transforms used for visualization.
-        /// </summary>
-        public IReadOnlyDictionary<TrackedHandJoint, Transform> JointTransforms => jointTransforms;
 
         /// <summary>
         /// Should a gizmo be drawn to represent the hand bounds.
@@ -97,15 +84,10 @@ namespace XRTK.SDK.UX.Controllers.Hands
             }
         }
 
-        /// <summary>
-        /// Executes when the visuailzer is disabled.
-        /// </summary>
+        /// <inheritdoc />
         protected override void OnDisable()
         {
             dataProvider.Unregister(this);
-            ClearJoints();
-            ClearMesh();
-
             base.OnDisable();
         }
 
@@ -122,124 +104,16 @@ namespace XRTK.SDK.UX.Controllers.Hands
             UpdateHandJointVisualization(handData);
         }
 
-        protected virtual void UpdateHandJointVisualization(HandData handData)
-        {
-            if (Profile == null || !Profile.EnableHandJointVisualization)
-            {
-                ClearJoints();
-            }
-            else
-            {
-                IReadOnlyDictionary<TrackedHandJoint, MixedRealityPose> jointPoses = HandUtils.ToJointPoseDictionary(handData.Joints);
-                foreach (TrackedHandJoint handJoint in jointPoses.Keys)
-                {
-                    if (jointTransforms.TryGetValue(handJoint, out Transform jointTransform))
-                    {
-                        jointTransform.position = jointPoses[handJoint].Position;
-                        jointTransform.rotation = jointPoses[handJoint].Rotation;
-                    }
-                    else if (handJoint != TrackedHandJoint.None)
-                    {
-                        CreateJoint(handJoint, jointPoses);
-                    }
-                }
-            }
-        }
-
-        protected virtual void UpdateHansMeshVisualization(HandData handData)
-        {
-            if (Profile == null || !Profile.EnableHandMeshVisualization || handData.Mesh == null)
-            {
-                ClearMesh();
-                return;
-            }
-
-            HandMeshData handMeshData = handData.Mesh;
-            if (handMeshData.Empty)
-            {
-                return;
-            }
-
-            if (MeshFilter == null && Profile?.HandMeshPrefab != null)
-            {
-                CreateMeshFilter();
-            }
-
-            if (MeshFilter != null)
-            {
-                Mesh mesh = MeshFilter.mesh;
-
-                mesh.vertices = handMeshData.Vertices;
-                mesh.normals = handMeshData.Normals;
-                mesh.triangles = handMeshData.Triangles;
-
-                if (handMeshData.Uvs != null && handMeshData.Uvs.Length > 0)
-                {
-                    mesh.uv = handMeshData.Uvs;
-                }
-
-                MeshFilter.transform.position = handMeshData.Position;
-                MeshFilter.transform.rotation = handMeshData.Rotation;
-            }
-        }
-
-        protected virtual void ClearJoints()
-        {
-            foreach (var joint in jointTransforms)
-            {
-                Destroy(joint.Value.gameObject);
-            }
-
-            jointTransforms.Clear();
-        }
-
-        protected virtual void CreateJoint(TrackedHandJoint handJoint, IReadOnlyDictionary<TrackedHandJoint, MixedRealityPose> jointPoses)
-        {
-            GameObject prefab = Profile.JointPrefab;
-            if (handJoint == TrackedHandJoint.Palm)
-            {
-                prefab = Profile.PalmJointPrefab;
-            }
-            else if (handJoint == TrackedHandJoint.IndexTip)
-            {
-                prefab = Profile.FingerTipPrefab;
-            }
-
-            GameObject jointObject;
-            if (prefab != null)
-            {
-                jointObject = Instantiate(prefab);
-            }
-            else
-            {
-                jointObject = new GameObject();
-            }
-
-            jointObject.name = handJoint.ToString() + " Proxy Transform";
-            jointObject.transform.position = jointPoses[handJoint].Position;
-            jointObject.transform.rotation = jointPoses[handJoint].Rotation;
-            jointObject.transform.parent = transform;
-
-            jointTransforms.Add(handJoint, jointObject.transform);
-        }
+        /// <summary>
+        /// Updates hand joint visulization with latest available hand data.
+        /// </summary>
+        /// <param name="handData">Updated hand data.</param>
+        protected abstract void UpdateHandJointVisualization(HandData handData);
 
         /// <summary>
-        /// Destroys the mesh filter used for hand controller mesh visualization.
+        /// Updates hand mesh visulization with latest available hand data.
         /// </summary>
-        protected virtual void ClearMesh()
-        {
-            if (MeshFilter != null)
-            {
-                Destroy(MeshFilter.gameObject);
-            }
-        }
-
-        /// <summary>
-        /// Creates the mesh filter used for hand controller mesh visualization.
-        /// </summary>
-        protected virtual void CreateMeshFilter()
-        {
-            MeshFilter = Instantiate(Profile.HandMeshPrefab).GetComponent<MeshFilter>();
-        }
+        /// <param name="handData">Updated hand data.</param>
+        protected abstract void UpdateHansMeshVisualization(HandData handData);
     }
 }
--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Controllers/Hands/DefaultHandControllerVisualizer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Controllers/Hands/DefaultHandControllerVisualizer.cs
@@ -1,10 +1,152 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
+using UnityEngine;
+using XRTK.Definitions.Utilities;
+using XRTK.Providers.Controllers.Hands;
+
 namespace XRTK.SDK.UX.Controllers.Hands
 {
     /// <summary>
     /// Default hand controller visualizer implementation.
     /// </summary>
-    public class DefaultHandControllerVisualizer : BaseHandControllerVisualizer { }
+    public class DefaultHandControllerVisualizer : BaseHandControllerVisualizer
+    {
+        private Dictionary<TrackedHandJoint, Transform> jointTransforms = new Dictionary<TrackedHandJoint, Transform>();
+
+        /// <summary>
+        /// The mesh filter used for visualization.
+        /// </summary>
+        public MeshFilter MeshFilter { get; private set; }
+
+        /// <summary>
+        /// Provides read-only access to the joint transforms used for visualization.
+        /// </summary>
+        public IReadOnlyDictionary<TrackedHandJoint, Transform> JointTransforms => jointTransforms;
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            ClearJoints();
+            ClearMesh();
+        }
+
+        /// <inheritdoc />
+        protected override void UpdateHandJointVisualization(HandData handData)
+        {
+            if (Profile == null || !Profile.EnableHandJointVisualization)
+            {
+                ClearJoints();
+            }
+            else
+            {
+                IReadOnlyDictionary<TrackedHandJoint, MixedRealityPose> jointPoses = HandUtils.ToJointPoseDictionary(handData.Joints);
+                foreach (TrackedHandJoint handJoint in jointPoses.Keys)
+                {
+                    if (jointTransforms.TryGetValue(handJoint, out Transform jointTransform))
+                    {
+                        jointTransform.position = jointPoses[handJoint].Position;
+                        jointTransform.rotation = jointPoses[handJoint].Rotation;
+                    }
+                    else if (handJoint != TrackedHandJoint.None)
+                    {
+                        CreateJoint(handJoint, jointPoses);
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void UpdateHansMeshVisualization(HandData handData)
+        {
+            if (Profile == null || !Profile.EnableHandMeshVisualization || handData.Mesh == null)
+            {
+                ClearMesh();
+                return;
+            }
+
+            HandMeshData handMeshData = handData.Mesh;
+            if (handMeshData.Empty)
+            {
+                return;
+            }
+
+            if (MeshFilter == null && Profile?.HandMeshPrefab != null)
+            {
+                CreateMeshFilter();
+            }
+
+            if (MeshFilter != null)
+            {
+                Mesh mesh = MeshFilter.mesh;
+
+                mesh.vertices = handMeshData.Vertices;
+                mesh.normals = handMeshData.Normals;
+                mesh.triangles = handMeshData.Triangles;
+
+                if (handMeshData.Uvs != null && handMeshData.Uvs.Length > 0)
+                {
+                    mesh.uv = handMeshData.Uvs;
+                }
+
+                MeshFilter.transform.position = handMeshData.Position;
+                MeshFilter.transform.rotation = handMeshData.Rotation;
+            }
+        }
+
+        private void CreateJoint(TrackedHandJoint handJoint, IReadOnlyDictionary<TrackedHandJoint, MixedRealityPose> jointPoses)
+        {
+            GameObject prefab = Profile.JointPrefab;
+            if (handJoint == TrackedHandJoint.Palm)
+            {
+                prefab = Profile.PalmJointPrefab;
+            }
+            else if (handJoint == TrackedHandJoint.IndexTip)
+            {
+                prefab = Profile.FingerTipPrefab;
+            }
+
+            GameObject jointObject;
+            if (prefab != null)
+            {
+                jointObject = Instantiate(prefab);
+            }
+            else
+            {
+                jointObject = new GameObject();
+            }
+
+            jointObject.name = handJoint.ToString() + " Proxy Transform";
+            jointObject.transform.position = jointPoses[handJoint].Position;
+            jointObject.transform.rotation = jointPoses[handJoint].Rotation;
+            jointObject.transform.parent = transform;
+
+            jointTransforms.Add(handJoint, jointObject.transform);
+        }
+
+        private void ClearJoints()
+        {
+            foreach (var joint in jointTransforms)
+            {
+                Destroy(joint.Value.gameObject);
+            }
+
+            jointTransforms.Clear();
+        }
+
+        private void ClearMesh()
+        {
+            if (MeshFilter != null)
+            {
+                Destroy(MeshFilter.gameObject);
+            }
+        }
+
+        private void CreateMeshFilter()
+        {
+            MeshFilter = Instantiate(Profile.HandMeshPrefab).GetComponent<MeshFilter>();
+        }
+    }
 }
