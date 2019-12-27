6f5f8480a97a8278a2f804fa6527a37311b2a835
Stephen Hodgson <hodgson.designs@gmail.com>
2019-12-27 16:39:44 -0500

    added console text color options

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/Diagnostics/Scripts/MixedRealityConsoleDiagnosticsHandler.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/Diagnostics/Scripts/MixedRealityConsoleDiagnosticsHandler.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using XRTK.Attributes;
 using XRTK.EventDatum.DiagnosticsSystem;
 using XRTK.Interfaces.DiagnosticsSystem.Handlers;
@@ -14,6 +14,15 @@ namespace XRTK.SDK.DiagnosticsSystem
         IMixedRealityConsoleDiagnosticsHandler
     {
         [SerializeField]
+        private Color logEntryTextColor = Color.black;
+
+        [SerializeField]
+        private Color warningEntryTextColor = Color.yellow;
+
+        [SerializeField]
+        private Color errorEntryTextColor = Color.red;
+
+        [SerializeField]
         [Tooltip("Maximum allowed entries displayed in the log console.")]
         private int maxEntries = 10;
 
@@ -23,22 +32,72 @@ namespace XRTK.SDK.DiagnosticsSystem
         private GameObject logTextPrefab = null;
 
         [SerializeField]
-        [Tooltip("The scroll view displaying log text.")]
-        private ScrollRect logScrollView = null;
-
-        [SerializeField]
         [Tooltip("The scroll view's content root transform to spawn console text entries onto.")]
         private Transform contentRoot = null;
 
-        private TextMeshProUGUI[] textContainers;
+        private MessageEntry[] textContainers;
+
+        private class MessageEntry
+        {
+            public MessageEntry(MixedRealityConsoleDiagnosticsHandler handler, TextMeshProUGUI textContent)
+            {
+                this.handler = handler;
+                this.textContent = textContent;
+            }
+
+            private readonly TextMeshProUGUI textContent;
+            private readonly MixedRealityConsoleDiagnosticsHandler handler;
+
+            public string Message
+            {
+                get => textContent.text;
+                set
+                {
+                    if (textContent.text == value) { return; }
+                    textContent.gameObject.SetActive(false);
+                    textContent.text = value;
+                    textContent.gameObject.SetActive(true);
+                }
+            }
+
+            private LogType logType;
+
+            public LogType LogType
+            {
+                get => logType;
+                set
+                {
+                    if (logType == value) { return; }
+
+                    switch (value)
+                    {
+                        case LogType.Assert:
+                        case LogType.Error:
+                        case LogType.Exception:
+                            textContent.color = handler.errorEntryTextColor;
+                            break;
+                        case LogType.Warning:
+                            textContent.color = handler.warningEntryTextColor;
+                            break;
+                        case LogType.Log:
+                            textContent.color = handler.logEntryTextColor;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(value), value, null);
+                    }
+
+                    logType = value;
+                }
+            }
+        }
 
         private void Awake()
         {
-            textContainers = new TextMeshProUGUI[maxEntries];
+            textContainers = new MessageEntry[maxEntries];
 
             for (int i = 0; i < textContainers.Length; i++)
             {
-                textContainers[i] = Instantiate(logTextPrefab, contentRoot).GetComponent<TextMeshProUGUI>();
+                textContainers[i] = new MessageEntry(this, Instantiate(logTextPrefab, contentRoot).GetComponent<TextMeshProUGUI>());
             }
         }
 
@@ -49,15 +108,17 @@ namespace XRTK.SDK.DiagnosticsSystem
 
             for (entryIndex = textContainers.Length - 1; entryIndex > 0; entryIndex--)
             {
-                textContainers[entryIndex].text = textContainers[entryIndex - 1].text;
+                var entry = textContainers[entryIndex];
+                var prevEntry = textContainers[entryIndex - 1];
+                entry.Message = prevEntry.Message;
+                entry.LogType = prevEntry.LogType;
             }
 
             if (entryIndex == 0)
             {
-                textContainers[entryIndex].text = eventData.Message;
+                textContainers[0].Message = eventData.Message;
+                textContainers[0].LogType = eventData.LogType;
             }
-
-            logScrollView.verticalNormalizedPosition = 0f;
         }
     }
 }
