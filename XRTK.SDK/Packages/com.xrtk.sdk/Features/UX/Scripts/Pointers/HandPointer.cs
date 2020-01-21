c9dacd60d33ae573217bae80279763493fba71b3
Dino Fejzagic <dino.f@live.de>
2020-01-21 20:27:01 +0100

    Create hand pointer

---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/HandPointer.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace XRTK.SDK.UX.Pointers
+{
+    /// <summary>
+    /// Pointer class for hand controller pointers.
+    /// </summary>
+    public class HandPointer : BaseControllerPointer
+    {
+
+    }
+}
