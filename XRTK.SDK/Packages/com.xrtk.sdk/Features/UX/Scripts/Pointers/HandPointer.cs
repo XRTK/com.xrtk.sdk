b66c1db92a899770242bfc67c6a9f5a186a4445b
Stephen Hodgson <hodgson.designs@gmail.com>
2020-03-29 15:01:57 -0400

    Removed hand pointer and added references to correct hand data provider profiles

--- XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/HandPointer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Features/UX/Scripts/Pointers/HandPointer.cs
@@ -1,11 +1,5 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using System;
-using UnityEngine;
-using XRTK.Definitions.Utilities;
-using XRTK.Interfaces.InputSystem.Controllers.Hands;
-using XRTK.Services;
 
 namespace XRTK.SDK.UX.Pointers
 {
@@ -14,52 +8,34 @@ namespace XRTK.SDK.UX.Pointers
     /// </summary>
     public class HandPointer : BaseControllerPointer
     {
-        private IMixedRealityHandRay handRay;
+        //private IMixedRealityHandRay handRay;
 
-        /// <inheritdoc />
-        protected override void OnEnable()
-        {
-            base.OnEnable();
+        ///// <inheritdoc />
+        //protected override void OnEnable()
+        //{
+        //    base.OnEnable();
 
-            if (handRay == null)
-            {
-                InitializeHandRay();
-            }
-        }
+        //    if (handRay == null)
+        //    {
+        //        InitializeHandRay();
+        //    }
+        //}
 
-        /// <inheritdoc />
-        protected override void Start()
-        {
-            base.Start();
+        ///// <inheritdoc />
+        //protected override void Start()
+        //{
+        //    base.Start();
 
-            if (handRay == null)
-            {
-                InitializeHandRay();
-            }
-        }
+        //    if (handRay == null)
+        //    {
+        //        InitializeHandRay();
+        //    }
+        //}
 
-        /// <inheritdoc />
-        public override bool TryGetPointerPosition(out Vector3 position)
-        {
-            return base.TryGetPointerPosition(out position);
-        }
-
-        /// <inheritdoc />
-        public override bool TryGetPointerRotation(out Quaternion rotation)
-        {
-            return base.TryGetPointerRotation(out rotation);
-        }
-
-        /// <inheritdoc />
-        public override bool TryGetPointingRay(out Ray pointingRay)
-        {
-            return base.TryGetPointingRay(out pointingRay);
-        }
-
-        private void InitializeHandRay()
-        {
-            SystemType handRayType = MixedRealityToolkit.Instance.ActiveProfile.InputSystemProfile.HandTrackingProfile.HandRayType;
-            handRay = (IMixedRealityHandRay)Activator.CreateInstance(handRayType.Type);
-        }
+        //private void InitializeHandRay()
+        //{
+        //    SystemType handRayType = MixedRealityToolkit.Instance.ActiveProfile.InputSystemProfile.HandTrackingProfile.HandRayType;
+        //    handRay = (IMixedRealityHandRay)Activator.CreateInstance(handRayType.Type);
+        //}
     }
 }
