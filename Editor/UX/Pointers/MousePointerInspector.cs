792fbe28f4b41b1b9d79551d1e0b73a843f9f36e
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-27 02:38:15 -0400

    Dev/package layout shuffle (#163)
    * Input Action Refactor
    
    Updated profiles
    
    * added guid references to actions
    
    * updated the input handler inspectors
    
    * updated all the default controller mappings
    
    * updated serialization
    
    * bumped package version
    
    * removed some profiles and updated the gaze provider
    
    * removed unused profile and folder
    
    * revert package changes
    
    * updated profile references
    
    * removed old profiles
    
    * updated profile references
    
    * removed empty folder
    
    * updated default controller profiles
    
    * removed a bunch of created profiles
    
    * Rolled back registered processors
    
    Added default mouse mappings
    
    * Added default touch and unity joystick controllers
    
    * removed empty array
    
    * updated all the controller profiles and their mappings
    
    * updated controller visualizations
    
    * removed some meta files
    
    * updated changes from broken out PRs
    
    * updated speech input data provider's registration
    
    * updated input system profile serialization
    
    * updated spatial awareness system profile serialization
    
    * updated handler references
    
    * fixed some issues with the touch controller
    
    * Added license header to controller visualizer
    
    * Added a spatial pointer pose interaction mapping to both left/right articulated hand controllers
    
    * updated diagnostics profile serialization
    
    * Added base camera data provider and updated references to camera system
    
    * updated camera settings
    
    * renamed default camera system profile
    
    * updated name property
    
    * removed extra hololens interaction mappings
    
    * removed empty reference
    
    * updated service configurations
    
    * updated serailization
    
    * updated serailization
    
    * moved Lumin camera data provider profile to it's package
    
    * updated default camera data provider profile
    
    * updated camera data provider configurations
    
    * fixed lumin camera data provider reference
    
    * updated reference to wmr profile
    
    * Fixed the hand controller visualizations
    
    Position should be in local space
    
    * updated camera system settings
    
    * fixed data provider serialization
    
    * removed unused asset file
    
    * Updated windows controller type name to get the controller textures to show up correctly based on the type name
    
    * updated camera system profile serialization
    
    * updated simulated controller type name to make texture lookup work
    
    * Added Oculus Camera Data Provider
    
    * cleaned up wmr interactions
    
    * Fixed platform references for input data providers
    
    * fixed gaze provider
    
    * specified oculus platform for controllers
    
    * fixed profile runtime platform definitions
    
    * updated references from moving the wmr package into the correct layout
    
    * updated some references to profiles
    
    * fixed open vr controller data provider reference
    
    * updated lumin references
    
    * updated the editor namespaces
    
    Co-authored-by: Stephen Hodgson <StephenHodgson@users.noreply.github.com>

---
+++ Editor/UX/Pointers/MousePointerInspector.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using UnityEditor;
+using UnityEngine;
+using XRTK.Editor.Extensions;
+using XRTK.SDK.UX.Pointers;
+
+namespace XRTK.SDK.Editor.UX.Pointers
+{
+    [CustomEditor(typeof(MousePointer))]
+    public class MousePointerInspector : BaseControllerPointerInspector
+    {
+        private readonly GUIContent mousePointerFoldoutContent = new GUIContent("Mouse Pointer Settings");
+
+        private SerializedProperty hideCursorWhenInactive;
+        private SerializedProperty hideTimeout;
+        private SerializedProperty movementThresholdToUnHide;
+        private SerializedProperty speed;
+
+        protected override void OnEnable()
+        {
+            DrawBasePointerActions = false;
+            base.OnEnable();
+
+            hideCursorWhenInactive = serializedObject.FindProperty(nameof(hideCursorWhenInactive));
+            movementThresholdToUnHide = serializedObject.FindProperty(nameof(movementThresholdToUnHide));
+            hideTimeout = serializedObject.FindProperty(nameof(hideTimeout));
+            speed = serializedObject.FindProperty(nameof(speed));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update();
+
+            if (hideCursorWhenInactive.FoldoutWithBoldLabelPropertyField(mousePointerFoldoutContent))
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(hideCursorWhenInactive);
+
+                if (hideCursorWhenInactive.boolValue)
+                {
+                    EditorGUILayout.PropertyField(hideTimeout);
+                    EditorGUILayout.PropertyField(movementThresholdToUnHide);
+                }
+
+                EditorGUI.indentLevel--;
+
+                EditorGUILayout.PropertyField(speed);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
