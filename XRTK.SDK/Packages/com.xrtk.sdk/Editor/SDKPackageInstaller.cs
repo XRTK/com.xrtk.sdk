0e8834eb8d47f101ecc5423ad20175852239bd9c
Stephen Hodgson <hodgson.designs@gmail.com>
2020-04-30 08:12:55 -0400

    Added package installer

---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Editor/SDKPackageInstaller.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using UnityEditor;
+using XRTK.Editor;
+using XRTK.Extensions;
+using XRTK.Utilities.Editor;
+
+namespace XRTK.SDK.Editor
+{
+    [InitializeOnLoad]
+    internal static class SDKPackageInstaller
+    {
+        private static readonly string DefaultPath = $"{MixedRealityPreferences.ProfileGenerationPath}SDK\\";
+        private static readonly string HiddenPath = Path.GetFullPath($"{PathFinderUtility.ResolvePath<IPathFinder>(typeof(SdkPathFinder)).ToForwardSlashes()}\\{MixedRealityPreferences.HIDDEN_PROFILES_PATH}");
+
+        static SDKPackageInstaller()
+        {
+            if (!EditorPreferences.Get($"{nameof(SDKPackageInstaller)}", false))
+            {
+                EditorPreferences.Set($"{nameof(SDKPackageInstaller)}", PackageInstaller.TryInstallProfiles(HiddenPath, DefaultPath));
+            }
+        }
+    }
+}
