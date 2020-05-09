134ea10abfc5e6dcb3b88746b99c2876be94e439
Stephen Hodgson <hodgson.designs@gmail.com>
2020-05-09 05:53:23 -0400

    cleaned up sdk a bit (#178)
    * cleaned up sdk a bit
    
    * Moved GazeProvider into core
    
    cleaned up headers and references

--- XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Pointers/TouchPointer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Pointers/TouchPointer.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using UnityEngine;
 using XRTK.EventDatum.Input;
 using XRTK.Interfaces.InputSystem;
 using XRTK.Utilities;
 using XRTK.Utilities.Physics;
-using UnityEngine;
 
 namespace XRTK.SDK.UX.Pointers
 {
--- XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Tooltips/ToolTip.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Tooltips/ToolTip.cs
@@ -2,9 +2,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 //
-using XRTK.Utilities.Lines.DataProviders;
+
 using System;
 using UnityEngine;
+using XRTK.Utilities.Lines.DataProviders;
 
 namespace XRTK.SDK.UX.ToolTips
 {
