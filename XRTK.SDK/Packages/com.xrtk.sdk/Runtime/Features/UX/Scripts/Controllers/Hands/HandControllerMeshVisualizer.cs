1fb735228cd732bd8b95d0408593e4acaaca856c
Dino Fejzagic <dino.f@live.de>
2020-05-10 20:08:29 +0200

    Update prefabs

--- XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Controllers/Hands/HandControllerMeshVisualizer.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Controllers/Hands/HandControllerMeshVisualizer.cs
@@ -8,6 +8,8 @@ namespace XRTK.SDK.UX.Controllers.Hands
 {
     public class HandControllerMeshVisualizer : MonoBehaviour
     {
+        private Vector3[] lastHandMeshVertices;
+
         [SerializeField]
         private MeshFilter meshFilter = null;
 
@@ -26,17 +28,40 @@ namespace XRTK.SDK.UX.Controllers.Hands
             if (meshFilter != null)
             {
                 Mesh mesh = meshFilter.mesh;
+                if (lastHandMeshVertices == null)
+                {
+                    lastHandMeshVertices = mesh.vertices;
+                }
+
+                bool meshChanged = false;
+                // On some platforms, mesh length counts may change as the hand mesh is updated.
+                // In order to update the vertices when the array sizes change, the mesh
+                // must be cleared per instructions here:
+                // https://docs.unity3d.com/ScriptReference/Mesh.html
+                if (lastHandMeshVertices != null &&
+                    lastHandMeshVertices.Length != 0 &&
+                    lastHandMeshVertices.Length != handMeshData.Vertices.Length)
+                {
+                    meshChanged = true;
+                    mesh.Clear();
+                }
 
                 mesh.vertices = handMeshData.Vertices;
                 mesh.normals = handMeshData.Normals;
-                mesh.triangles = handMeshData.Triangles;
+                lastHandMeshVertices = mesh.vertices;
 
-                if (handMeshData.Uvs != null && handMeshData.Uvs.Length > 0)
+                if (meshChanged)
                 {
-                    mesh.uv = handMeshData.Uvs;
+                    mesh.triangles = handMeshData.Triangles;
+                    if (handMeshData.Uvs != null && handMeshData.Uvs.Length > 0)
+                    {
+                        mesh.uv = handMeshData.Uvs;
+                    }
+
+                    mesh.RecalculateBounds();
                 }
 
-                meshFilter.transform.position = handMeshData.Position;
+                //meshFilter.transform.position = handMeshData.Position;
                 meshFilter.transform.rotation = handMeshData.Rotation;
             }
         }
