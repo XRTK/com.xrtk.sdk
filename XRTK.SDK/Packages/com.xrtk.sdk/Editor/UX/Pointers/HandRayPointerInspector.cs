ce77dfb63dba0de1d0ca1254a44f649bb881de9f
Dino Fejzagic <dino.f@live.de>
2020-05-18 15:18:19 +0200

    Create custom pointer prefab for hands

---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Editor/UX/Pointers/HandRayPointerInspector.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using XRTK.SDK.UX.Pointers;
+
+namespace XRTK.SDK.Editor.UX.Pointers
+{
+    [CustomEditor(typeof(HandRayPointer))]
+    public class HandRayPointerInspector : LinePointerInspector
+    {
+
+    }
+}
---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/UX/Scripts/Pointers/HandRayPointer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Definitions.Controllers.Hands;
+using XRTK.EventDatum.Input;
+using XRTK.Interfaces.Providers.Controllers.Hands;
+
+namespace XRTK.SDK.UX.Pointers
+{
+    /// <summary>
+    /// Extends the simple line pointer for drawing lines from the input source origin to the current pointer position,
+    /// by adding visual state
+    /// </summary>
+    public class HandRayPointer : LinePointer
+    {
+        /// <inheritdoc />
+        public override void OnInputChanged(InputEventData<HandData> eventData)
+        {
+            base.OnInputChanged(eventData);
+
+            // We listen for hand data changes here, so we can adjust the pointer's
+            // state if needed.
+            if (Controller is IMixedRealityHandController handController)
+            {
+                enabled = handController.IsPointing;
+            }
+            else
+            {
+                Debug.LogError($"{typeof(HandRayPointer).Name} is only for use with {typeof(IMixedRealityHandController).Name} controllers!", this);
+            }
+        }
+    }
+}
