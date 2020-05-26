9b3c0591585c829d88a60285e24e3fe3c11562be
Dino Fejzagic <dino.f@live.de>
2020-05-26 18:01:50 +0200

    Add debug utility to record hand data

---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/Input/HandDataRecorder.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using XRTK.Definitions.Controllers.Hands;
+using XRTK.Definitions.Utilities;
+using XRTK.EventDatum.Input;
+using XRTK.Interfaces.InputSystem.Handlers;
+
+namespace XRTK.SDK.Input
+{
+    /// <summary>
+    /// Utilitiy component to record a hand controller's data into a file.
+    /// </summary>
+    public class HandDataRecorder : InputSystemGlobalListener, IMixedRealityInputHandler<HandData>
+    {
+        private RecordedHandJoints currentRecording;
+
+        [SerializeField]
+        [Tooltip("The handedness of the hand to record data for.")]
+        private Handedness targetHandedness = Handedness.Right;
+
+        [SerializeField]
+        [Tooltip("Keycode to trigger saving of the currently recorded data.")]
+        private KeyCode saveRecordingKey = KeyCode.Return;
+
+        private void Update()
+        {
+            if (currentRecording != null && UnityEngine.Input.GetKeyDown(saveRecordingKey))
+            {
+                Debug.Log(JsonUtility.ToJson(currentRecording));
+            }
+        }
+
+        public void OnInputChanged(InputEventData<HandData> eventData)
+        {
+#if UNITY_EDITOR
+
+            if (targetHandedness != eventData.Handedness)
+            {
+                return;
+            }
+
+            var handData = eventData.InputData;
+            var recordedHandJoints = new RecordedHandJoints();
+            var jointPoses = new RecordedHandJoint[HandData.JointCount];
+
+            for (int i = 0; i < HandData.JointCount; i++)
+            {
+                var jointPose = handData.Joints[i];
+                jointPoses[i] = new RecordedHandJoint((TrackedHandJoint)i, jointPose);
+            }
+
+            recordedHandJoints.items = jointPoses;
+            currentRecording = recordedHandJoints;
+
+#endif
+        }
+    }
+}
