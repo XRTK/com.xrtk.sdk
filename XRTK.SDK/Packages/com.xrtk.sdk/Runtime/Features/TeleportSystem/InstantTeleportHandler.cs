c0afc7ce287573360d67b7a98d5fa5a5b6d99d8a
Dino Fejzagić <dino.f@live.de>
2020-12-17 19:17:04 +0100

    Implement instant teleport handler

--- XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/TeleportSystem/InstantTeleportHandler.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/TeleportSystem/InstantTeleportHandler.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using UnityEngine;
 using XRTK.EventDatum.Teleport;
+using XRTK.Extensions;
 using XRTK.Interfaces.TeleportSystem;
 
 namespace XRTK.SDK.TeleportSystem
@@ -10,19 +12,68 @@ namespace XRTK.SDK.TeleportSystem
     /// This <see cref="IMixedRealityTeleportSystem"/> handler implementation will
     /// perform an instant teleport to the target location.
     /// </summary>
+    [RequireComponent(typeof(Camera))]
     [System.Runtime.InteropServices.Guid("cb3ee7a8-114e-44d6-9edc-cd55049fefb6")]
     public class InstantTeleportHandler : BaseTeleportHandler
     {
-        /// <inheritdoc />
-        public override void OnTeleportCanceled(TeleportEventData eventData) { }
+        [SerializeField]
+        private Transform cameraTransform = null;
+
+        [SerializeField]
+        private Transform parentTransform = null;
+
+        private Vector3 targetPosition;
+        private Vector3 targetRotation;
+
+        /// <summary>
+        /// Awake is called when the instance is being loaded.
+        /// </summary>
+        private void Awake()
+        {
+            if (cameraTransform.IsNull())
+            {
+                cameraTransform = transform;
+            }
+
+            if (parentTransform.IsNull())
+            {
+                parentTransform = cameraTransform.parent;
+                Debug.Assert(parentTransform != null,
+                    $"{nameof(InstantTeleportHandler)} requires that the camera be parented under another object " +
+                    $"or a parent transform was assigned in editor.");
+            }
+        }
 
         /// <inheritdoc />
-        public override void OnTeleportCompleted(TeleportEventData eventData) { }
+        public override void OnTeleportStarted(TeleportEventData eventData)
+        {
+            if (eventData.used)
+            {
+                return;
+            }
 
-        /// <inheritdoc />
-        public override void OnTeleportRequest(TeleportEventData eventData) { }
+            eventData.Use();
 
-        /// <inheritdoc />
-        public override void OnTeleportStarted(TeleportEventData eventData) { }
+            targetRotation = Vector3.zero;
+            targetPosition = eventData.Pointer.Result.EndPoint;
+            targetRotation.y = eventData.Pointer.PointerOrientation;
+
+            if (eventData.HotSpot != null)
+            {
+                targetPosition = eventData.HotSpot.Position;
+                if (eventData.HotSpot.OverrideTargetOrientation)
+                {
+                    targetRotation.y = eventData.HotSpot.TargetOrientation;
+                }
+            }
+
+            var height = targetPosition.y;
+            targetPosition -= cameraTransform.position - parentTransform.position;
+            targetPosition.y = height;
+            parentTransform.position = targetPosition;
+            parentTransform.RotateAround(cameraTransform.position, Vector3.up, targetRotation.y - cameraTransform.eulerAngles.y);
+
+            TeleportSystem.RaiseTeleportComplete(eventData.Pointer, eventData.HotSpot);
+        }
     }
 }
