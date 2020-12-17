26c46851de03cce8d92a86be992cc256c74aed65
Dino Fejzagić <dino.f@live.de>
2020-12-17 18:31:20 +0100

    Introduce first set of teleport handlers

---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/TeleportSystem/FadingTeleportHandler.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.EventDatum.Teleport;
+using XRTK.Interfaces.TeleportSystem;
+
+namespace XRTK.SDK.TeleportSystem
+{
+    /// <summary>
+    /// This <see cref="IMixedRealityTeleportSystem"/> handler implementation will
+    /// fade out the camera when teleporting and fade it back in when done.
+    /// </summary>
+    [System.Runtime.InteropServices.Guid("0db5b0fd-9ac3-487a-abfd-754963f4e2a3")]
+    public class FadingTeleportHandler : BaseTeleportHandler
+    {
+        /// <inheritdoc />
+        public override void OnTeleportCanceled(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportCompleted(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportRequest(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportStarted(TeleportEventData eventData) { }
+    }
+}
---
+++ XRTK.SDK/Packages/com.xrtk.sdk/Runtime/Features/TeleportSystem/InstantTeleportHandler.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using XRTK.EventDatum.Teleport;
+using XRTK.Interfaces.TeleportSystem;
+
+namespace XRTK.SDK.TeleportSystem
+{
+    /// <summary>
+    /// This <see cref="IMixedRealityTeleportSystem"/> handler implementation will
+    /// perform an instant teleport to the target location.
+    /// </summary>
+    [System.Runtime.InteropServices.Guid("cb3ee7a8-114e-44d6-9edc-cd55049fefb6")]
+    public class InstantTeleportHandler : BaseTeleportHandler
+    {
+        /// <inheritdoc />
+        public override void OnTeleportCanceled(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportCompleted(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportRequest(TeleportEventData eventData) { }
+
+        /// <inheritdoc />
+        public override void OnTeleportStarted(TeleportEventData eventData) { }
+    }
+}
