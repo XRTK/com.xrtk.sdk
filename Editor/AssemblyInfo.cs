67334c35fc55f4e7b8f03ca87fcadd88d490e13d
Stephen Hodgson <hodgson.designs@gmail.com>
2021-05-16 13:36:26 -0400

    updated assembly info

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,4 +1,11 @@
-﻿// Copyright (c) XRTK. All rights reserved.
+// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #define XRTK_SDK_EDITOR
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.2.6")]
+[assembly: AssemblyTitle("com.xrtk.sdk.editor")]
+[assembly: AssemblyCompany("XRTK")]
+[assembly: AssemblyCopyright("Copyright (c) XRTK. All rights reserved.")]
