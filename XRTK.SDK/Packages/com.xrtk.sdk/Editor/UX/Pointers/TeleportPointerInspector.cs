1aefa8d7c415b767e5a44fcadc62afaef0e3c5dd
Dino Fejzagić <dino.f@live.de>
2021-09-04 17:49:45 +0200

    New Locomotion System (#243)
    # XRTK - Mixed Reality Toolkit Pull Request
    
    ## Overview
    This PR sets up profiles to work with the new locomotion system and contains related changes the locomotion system.
    Please visit the Core Module PR https://github.com/XRTK/XRTK-Core/pull/876 for more information.
    
    Co-authored-by: Stephen Hodgson <hodgson.designs@gmail.com>

--- XRTK.SDK/Packages/com.xrtk.sdk/Editor/UX/Pointers/TeleportPointerInspector.cs
+++ XRTK.SDK/Packages/com.xrtk.sdk/Editor/UX/Pointers/TeleportPointerInspector.cs
@@ -13,28 +13,14 @@ namespace XRTK.SDK.Editor.UX.Pointers
     {
         private readonly GUIContent teleportFoldoutHeader = new GUIContent("Teleport Pointer Settings");
 
-        private SerializedProperty inputThreshold;
-        private SerializedProperty angleOffset;
-        private SerializedProperty teleportActivationAngle;
-        private SerializedProperty rotateActivationAngle;
-        private SerializedProperty rotationAmount;
-        private SerializedProperty backStrafeActivationAngle;
-        private SerializedProperty strafeAmount;
-        private SerializedProperty lineColorHotSpot;
+        private SerializedProperty lineColorAnchor;
 
         protected override void OnEnable()
         {
             DrawBasePointerActions = false;
             base.OnEnable();
 
-            inputThreshold = serializedObject.FindProperty(nameof(inputThreshold));
-            angleOffset = serializedObject.FindProperty(nameof(angleOffset));
-            teleportActivationAngle = serializedObject.FindProperty(nameof(teleportActivationAngle));
-            rotateActivationAngle = serializedObject.FindProperty(nameof(rotateActivationAngle));
-            rotationAmount = serializedObject.FindProperty(nameof(rotationAmount));
-            backStrafeActivationAngle = serializedObject.FindProperty(nameof(backStrafeActivationAngle));
-            strafeAmount = serializedObject.FindProperty(nameof(strafeAmount));
-            lineColorHotSpot = serializedObject.FindProperty(nameof(lineColorHotSpot));
+            lineColorAnchor = serializedObject.FindProperty(nameof(lineColorAnchor));
         }
 
         public override void OnInspectorGUI()
@@ -42,17 +28,10 @@ namespace XRTK.SDK.Editor.UX.Pointers
             base.OnInspectorGUI();
             serializedObject.Update();
 
-            if (inputThreshold.FoldoutWithBoldLabelPropertyField(teleportFoldoutHeader))
+            if (lineColorAnchor.FoldoutWithBoldLabelPropertyField(teleportFoldoutHeader))
             {
                 EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(inputThreshold);
-                EditorGUILayout.PropertyField(angleOffset);
-                EditorGUILayout.PropertyField(teleportActivationAngle);
-                EditorGUILayout.PropertyField(rotateActivationAngle);
-                EditorGUILayout.PropertyField(rotationAmount);
-                EditorGUILayout.PropertyField(backStrafeActivationAngle);
-                EditorGUILayout.PropertyField(strafeAmount);
-                EditorGUILayout.PropertyField(lineColorHotSpot);
+                EditorGUILayout.PropertyField(lineColorAnchor);
                 EditorGUI.indentLevel--;
             }
 
